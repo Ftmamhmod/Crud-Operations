644f16912e9d2a95606bcdab6668769d8e82f072
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-08-10 16:54:24 +0300

    Home page commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,16 +4,13 @@ import { PiGraduationCap } from "react-icons/pi";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Link } from "react-router-dom";
-import userImg from "./../../assets/images/pexels-photo-2379004 1.png";
+import { userImg } from "./../../assets/index.js";
 
 export const SideBar = () => {
-
-const Name= localStorage.getItem("userEmail")
-
-
+  const Name = localStorage.getItem("userEmail");
   return (
-    <Sidebar className="h-screen w-[250px] fixed left-0 top-0 shadow-lg bg-white">
-      <h4 className="text-gray-800 pt-2 pb-5">
+    <Sidebar className="min-h-screen h-full w-[250px] fixed left-0 top-0 shadow-lg bg-white">
+      <h4 className="text-gray-800 py-5 my-6">
         <span className="text-yellow-500 p-2">
           <b>|</b>
         </span>
@@ -28,7 +25,10 @@ const Name= localStorage.getItem("userEmail")
         {/* <h6 className="p-2">{Name}</h6> */}
         <p className="text-yellow-500 pt-3">{Name}</p>
       </div>
-      <Menu className="flex flex-col justify-center h-auto items-center mt-6" style={{ lineHeight: '2' }}>
+      <Menu
+        className="flex flex-col justify-center h-auto items-center mt-6"
+        style={{ lineHeight: "2" }}
+      >
         <MenuItem
           icon={<IoHomeOutline className="text-lg" />}
           component={<Link to="/dashboard/home" />}
--- src/shared/NavBar/NavBar.jsx
+++ src/shared/NavBar/NavBar.jsx
@@ -1,15 +1,18 @@
 export const NavBar = () => {
   return (
     <nav className="navbar ">
-      <div className="">
-        <form className="flex justify-between w-full p-4" role="search">
+      <div className="shadow-lg">
+        <form className="flex justify-between gap-6 w-full p-4" role="search">
           <input
-            className="form-control me-2"
+            className="outline-0 form-control me-2 border border-neutral-300 focus:border-neutral-500 rounded-md p-2
+            focus:flex-1 transition-all duration-300"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button className="btn ">Logout</button>
+          <button className="btn cursor-pointer transition-all duration-300">
+            Logout
+          </button>
         </form>
       </div>
     </nav>
