d55b6a60c42884fe6236b439b81d72155f875f85
ftmamhmod <ftmamhmod2@gmail.com>
2025-08-13 20:12:46 +0300

    fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import userImg from "./images/user-img.png";
 import heroBgImg from "./images/hero-bg.jpg";
 
 export { userImg, heroBgImg };
-=======
-import userImg from "./images/user-img.png";
-import heroBgImg from "./images/hero-bg.jpg";
-
-export { userImg, heroBgImg };
->>>>>>> 1f8a00aa15f543f12de7e413648dccd793c67587
--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-<<<<<<< HEAD
-});
-=======
+
   build: {
 
     chunkSizeWarningLimit: 1500,
@@ -40,4 +38,3 @@ export default defineConfig({
     exclude: ['@rollup/rollup-linux-x64-gnu']
   }
 });
->>>>>>> 1f8a00aa15f543f12de7e413648dccd793c67587
